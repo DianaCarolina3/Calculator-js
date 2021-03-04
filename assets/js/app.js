1b0067ed817e6452556291a9a7ead8bd8da73b00
DianaCarolina3 <dcaroli2015@gmail.com>
2021-03-04 17:02:58 -0500

    adding JavaScript function buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,51 @@
+const numbersButton = document.getElementsByName('data-number');
+const operationButton = document.getElementsByName('data-operation');
+const equalsButton = document.getElementsByName('data-equals')[0];
+const deleteButton = document.getElementsByName('data-delete');
+const clearAllButton = document.getElementsByName('data-clearAll');
+var result = document.getElementById('result');
 
 
+function clikNumbers() {
+  numbersButton.forEach(button => {
+    button.addEventListener('click',() => {
+      // addNumber(button.innerText)
+      // alert(button.innerText)
+    })
+  })
+}
 
+function clikOperation() {
+  operationButton.forEach(button => {
+    button.addEventListener('click',() => {
+      // selectOperation(button.innerText)
+      // alert(button.innerText)
+    })
+  })
+}
 
+function clikEquals() {
+  equalsButton.addEventListener('click',() => {
+      //calcular
+      compute()
+      //actializar display
+      upgradeDisplay()
+      // alert(button.innerText)
+    })
+}
+
+function clikDelete() {
+  deleteButton.addEventListener('click',() => {
+      clearOne()
+      upgradeDisplay()
+      alert(button.innerText)
+    })
+}
+
+function clikClearAll() {
+  clearAllButton.addEventListener('click',() => {
+      clearAll()
+      upgradeDisplay()
+      alert(button.innerText)
+  })
+}
