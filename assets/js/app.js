261f3842c55ecdf86da84586f969931fdfce777a
DianaCarolina3 <dcaroli2015@gmail.com>
2021-03-05 19:31:28 -0500

    adding event button delete and clear

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,35 +27,92 @@ function clikOperation() {
 }
 
 function clikEquals() {
-  equalsButton.addEventListener('click',() => {
+  equalsButton.forEach(button => {
+    button.addEventListener('click',() => {
       //calcular
       compute()
-      //actualizar display
+      // //actualizar display
       upgradeDisplay()
+      // alert(button.innerText)
     })
+  })
 }
 
 function clikDelete() {
-  deleteButton.addEventListener('click',() => {
+  deleteButton.forEach(button => {
+  button.addEventListener('click',() => {
       clearOne()
       upgradeDisplay()
     })
+  })
 }
 
 function clikClearAll() {
-  clearAllButton.addEventListener('click',() => {
+  clearAllButton.forEach(button => {
+    button.addEventListener('click',() => {
       clearAll()
       upgradeDisplay()
+    })
   })
 }
 
 //functions operations
+
+function selectOperation(opera) {
+  // currentOperation = currentOperation.toString() + opera.toString()
+  if (currentOperation === '') return;
+  if (currentOperation !== '') {
+    compute()
+  }
+  operation = opera.toString()
+  previousOperation = currentOperation
+  currentOperation = ''
+}
+
+function compute() {
+  var calculate;
+  const previous = parseFloat(previousOperation)
+  const current = parseFloat(currentOperation)
+  if (isNaN(current) || isNaN(previous)) return;
+  switch(operation) {
+    case '+':
+      calculate = previous + current
+      break
+    case '-':
+      calculate = previous - current
+      break
+    case '×':
+      calculate = previous * current
+      break
+    case '÷':
+      calculate = previous / current
+      break
+    default:
+      return
+  }
+  currentOperation = calculate
+  operation = undefined
+  previousOperation = ''
+}
+
 function addNumber(number) {
   //contatena los numeros, mas no los suma
   currentOperation = currentOperation.toString() + number.toString()
   upgradeDisplay()
 }
 
+function clearAll() {
+  currentOperation = '';
+  previousOperation = '';
+  operation = undefined;
+}
+
+function clearOne() {
+  currentOperation = currentOperation.toString().slice(0, -1)
+}
+
 function upgradeDisplay() {
   result.value = currentOperation
 }
+
+
